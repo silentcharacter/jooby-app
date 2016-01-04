d9c54e39c7596d4e7a19afbe0f688b77be9e6ead
Igolnikov Ilya <ilya.igolnikov@novardis.com>
2016-01-05 00:06:29 +0300

    Test for users creation

--- src/test/java/com/mycompany/AppTest.java
+++ src/test/java/com/mycompany/AppTest.java
@@ -1,17 +1,46 @@
 package com.mycompany;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mycompany.domain.User;
 import org.junit.Test;
+
+import java.io.File;
 
 /**
  * @author jooby generator
  */
 public class AppTest extends BaseTest {
 
+  private static String[] lastNames = {"Иванов", "Петров", "Сидоров", "Путин", "Медведев", "Лавров", "Ходарковский"};
+  private static String[] firstNames = {"Иван", "Петр", "Сидр", "Владимир", "Дмитрий", "Сергей", "Михаил"};
+
   @Test
   public void index() throws Exception {
-//    server.get("/")
-//        .expect(200)
-//        .header("Content-Type", "text/html;charset=UTF-8");
+    server.get("/")
+        .expect(200)
+        .header("Content-Type", "text/html;charset=UTF-8");
+  }
+
+  @Test
+  public void createUsers() throws Exception {
+    ObjectMapper mapper = new ObjectMapper();
+    User user = new User();
+    for (int i = 0; i < 30; i++) {
+      int n = Double.valueOf(Math.random() * firstNames.length).intValue();
+      user.firstName = firstNames[n];
+      n = Double.valueOf(Math.random() * lastNames.length).intValue();
+      user.lastName = lastNames[n];
+      n = Double.valueOf(Math.random() * 100).intValue();
+      user.email = "test" + n + "@test.com";
+      n = Double.valueOf(Math.random() * 10000).intValue();
+      user.password = "" + n;
+      //Object to JSON in file
+      mapper.writeValue(new File("user.json"), user);
+      //Object to JSON in String
+      String jsonInString = mapper.writeValueAsString(user);
+      server.post("/api/users").body(jsonInString, "application/json")
+              .expect(200);
+    }
   }
 
 }
--- src/test/java/com/mycompany/BaseTest.java
+++ src/test/java/com/mycompany/BaseTest.java
@@ -14,13 +14,13 @@ public class BaseTest {
    * One app/server for all the test of this class. If you want to start/stop a new server per test,
    * remove the static modifier and replace the {@link ClassRule} annotation with {@link Rule}.
    */
-  @ClassRule
-  public static AppRule app = new AppRule(new App());
+//  @ClassRule
+//  public static AppRule app = new AppRule(new App());
 
   /**
    * One client per test. It creates a new HTTP client per each of the test method you have.
    */
   @Rule
-  public Client server = new Client("http://localhost:8080");
+  public Client server = new Client("https://localhost:8443");
 
 }
