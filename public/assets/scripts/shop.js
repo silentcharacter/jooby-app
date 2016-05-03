4b59de5fc940170efc3abf1197cd84158b69681d
Ilya Igolnikov <ilya.igolnikov@zaelab.com>
2016-05-04 00:22:24 +0300

    Ajax cart

--- public/assets/scripts/shop.js
+++ public/assets/scripts/shop.js
@@ -1,9 +1,15 @@
 $(document).ready(function () {
-  $('[data-toggle="offcanvas"]').click(function () {
-    $('.row-offcanvas').toggleClass('active')
-  });
+    $('[data-toggle="offcanvas"]').click(function () {
+        $('.row-offcanvas').toggleClass('active')
+    });
 
-//  updateCart();
+    $('[data-toggle="popover"]').popover({
+        html : true,
+        content: function() {
+          return $('#popover_content_wrapper').html();
+        }
+      }
+    );
 });
 
 function updateCart() {
