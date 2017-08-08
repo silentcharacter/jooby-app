ed9386a4fc6c51691f77edd3c9e516d7d68b2de2
Ilya Igolnikov <ilya.igolnikov@zaelab.com>
2017-08-09 02:40:21 +0300

    popup

--- public/assets/scripts/shop.js
+++ public/assets/scripts/shop.js
@@ -45,7 +45,7 @@ $(document).ready(function () {
 
     //close popup
     $('.cd-popup').on('click', function(event){
-        if( $(event.target).is('.cd-popup-close') || $(event.target).is('.cd-popup') ) {
+        if( $(event.target).is('.cd-popup-close img') || $(event.target).is('.cd-popup') ) {
             event.preventDefault();
             $(this).removeClass('is-visible');
         }
