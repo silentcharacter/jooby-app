d7ac640d5ab8598f53e8eb7309f871aff3f88b3d
Ilya Igolnikov <ilya.igolnikov@zaelab.com>
2017-09-18 10:09:18 +0300

    Small mobile fixes

--- public/assets/scripts/shop.js
+++ public/assets/scripts/shop.js
@@ -122,10 +122,14 @@ SHOP = {
             url: '/cart',
             success: function (result) {
                 $(".cart-total").text(result.totalPrice);
-                if (result.totalPrice > 0)
+                if (result.totalPrice > 0) {
                     $(".basket-responsive").removeClass('disabled');
-                else
+                    $(".basket-responsive .cart-total").css('display', 'inline');
+                    $(".basket-responsive .cart-total").html(result.totalPrice + '&#8381;');
+                } else {
                     $(".basket-responsive").addClass('disabled');
+                    $(".basket-responsive .cart-total").css('display', 'none');
+                }
             }
         });
     },
@@ -252,36 +256,6 @@ $(document).ready(function () {
     SHOP.bindScrolls();
     SHOP.bindFixedMenu();
     $('.mobile-cart .spinner .btn').on('click', SHOP.modifyCart);
-
-//    $('.category .title img').each(function(){
-//                var $img = $(this);
-//                var imgID = $img.attr('id');
-//                var imgClass = $img.attr('class');
-//                var imgURL = $img.attr('src');
-//
-//                jQuery.get(imgURL, function(data) {
-//                    // Get the SVG tag, ignore the rest
-//                    var $svg = $(data).find('svg');
-//
-//                    // Add replaced image's ID to the new SVG
-//                    if(typeof imgID !== 'undefined') {
-//                        $svg = $svg.attr('id', imgID);
-//                    }
-//                    // Add replaced image's classes to the new SVG
-//                    if(typeof imgClass !== 'undefined') {
-//                        $svg = $svg.attr('class', imgClass+' replaced-svg');
-//                    }
-//
-//                    // Remove any invalid XML tags as per http://validator.w3.org
-//                    $svg = $svg.removeAttr('xmlns:a');
-//
-//                    // Replace image with new SVG
-//                    $img.replaceWith($svg);
-//
-//                }, 'xml');
-//
-//            });
-
 });
 
 $(window).on("load", function () {
